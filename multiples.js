6b45a977e94853fc3b2f2dc62d35c0afc08ab321
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-10 10:34:58 -1000

    Deleted an unnecessary if condition and cleaned up the formatting.

--- multiples.js
+++ multiples.js
@@ -1,19 +1,17 @@
+'use strict';
 /**
  * sumOfAMultiple() sums of the multiples of 3 and 5 for any number passed in
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
-exports.sumOfAMultiple = function( n ) {
-  var sum = 0;
+exports.sumOfAMultiple = function (n) {
+  let sum = 0;
 
-  for(let i = 0; i < n; i++) {
-    if (i%3 === 0 & i%5===0){
-      sum += i;
-    } else if (i%3 === 0 || i%5 === 0) {
+  for (let i = 0; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
   }
-
 
   return sum;
 };
